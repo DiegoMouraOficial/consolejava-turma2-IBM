07ef6e44df549070168882f71aec76ac45950237
Diego Moura <programmercodeoficial@gmail.com>
2022-09-03 11:46:16 -0300

    Alterei um erro na Reolução do exercicio 2

--- src/main/java/com/br/diego/console/LogicaApplication.java
+++ src/main/java/com/br/diego/console/LogicaApplication.java
@@ -5,146 +5,136 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.br.diego.console.models.Aluno;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.br.diego.console.models.Aluno;
 
 @SpringBootApplication
 public class LogicaApplication {
 
-	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	private static List<Aluno> alunos = new ArrayList<>();
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static List<Aluno> alunos = new ArrayList<>();
 
-	/**
-	 * @param args
-	 * @throws IOException
-	 * @throws NumberFormatException
-	 * @throws InterruptedException
-	 */
-	public static void main(String[] args) throws IOException, NumberFormatException, InterruptedException {
+    public static void main(String[] args) throws IOException, NumberFormatException, InterruptedException {
+        while (true) {
+            limparTela();
 
-		while (true) {
-			limparTela();
-			System.out.println("=====[Cadastro dos Alunos]=====");
+            System.out.println("===== [ Cadastro dos alunos ] =======");
+            System.out.println("Qual opção você deseja?");
+            System.out.println("1 - Cadastrar aluno");
+            System.out.println("2 - Mostrar relatório");
+            System.out.println("3 - sair");
 
-			// Mostrar opcoes para o usuario
-			System.out.println("Qual opção você deseja?");
-			System.out.println("1 - Cadastrar aluno");
-			System.out.println("2 - Mostrar aluno");
-			System.out.println("3 - Sair");
+            int opcao = 0;
 
-			// Le a opcao digitada pelo usuario
-			int opcao = 0;
-			try {
-				Integer.parseInt(reader.readLine());
-			} catch (Exception e) {
-			}
+            try {
+                opcao = Integer.parseInt(reader.readLine());
+            } catch (Exception e) {
+            }
 
-			var sair = false;
-			switch (opcao) {
-				case 1:
-					cadastroAluno();
-					break;
-				case 2:
-					mostrarAluno();
-					break;
-				case 3:
-					sair = true;
-					break;
-				default:
-					opcaoInvalida();
-					break;
-			}
+            limparTela();
 
-			if (sair)
-				break;
-		}
+            var sair = false;
+            switch (opcao) {
+                case 1:
+                    cadastroAluno();
+                    break;
+                case 2:
+                    mostrarAlunos();
+                    break;
+                case 3:
+                    sair = true;
+                    break;
+                default:
+                    opcaoInvalida();
+                    break;
+            }
 
-		// SpringApplication.run(LogicaApplication.class, args);
+            if (sair)
+                break;
+        }
 
-	}
+        // SpringApplication.run(LogicaApplication.class, args);
+    }
 
-	private static void limparTela() {
-		System.out.print("\033[H\033[2J");
-		System.out.flush();
-	}
+    private static void limparTela() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
-	private static void opcaoInvalida() throws IOException, NumberFormatException, InterruptedException {
-		mensagem("Opção inválida");
+    private static void opcaoInvalida() throws IOException, NumberFormatException, InterruptedException {
+        mensagem("Opção inválida");
+    }
 
-	}
+    private static void capturaNotasAluno(Aluno aluno) throws NumberFormatException, IOException, InterruptedException {
+        System.out.println("Digite a nota do(a) " + aluno.getNome());
+        if (aluno.getNotas() == null)
+            aluno.setNotas(new ArrayList<Float>());
 
-	private static void capturaNotasAluno(Aluno aluno) throws NumberFormatException, IOException, InterruptedException {
-		System.out.println("Digite o nota do(a)" + aluno.getNotas());
-		if (aluno.getNotas() == null)
-			aluno.setNotas(new ArrayList<Float>());
+        try {
+            aluno.getNotas().add(Float.parseFloat(reader.readLine()));
+        } catch (Exception e) {
+            mensagem("Nota inválida");
+            capturaNotasAluno(aluno);
+        }
 
-		try {
-			aluno.getNotas().add(Float.parseFloat(reader.readLine()));
-		} catch (Exception e) {
-			mensagem("Nota inválida");
-			capturaNotasAluno(aluno);
-		}
+        try {
+            System.out.println("Digite 1 para cadastrar mais notas ou 0 para finalizar o cadaastro");
+            int opcao = Integer.parseInt(reader.readLine());
+            if (opcao == 1)
+                capturaNotasAluno(aluno);
+            return;
+        } catch (Exception e) {
+            mensagem("Opção inválida, iniciando novo cadastro de nota");
+            capturaNotasAluno(aluno);
+        }
+    }
 
-		try {
-			System.out.println("Digite 1 para cadastrar mais notas ou 0 para finalizar o cadastro");
-			int opcao = Integer.parseInt(reader.readLine());
-			if (opcao == 1)
-				capturaNotasAluno(aluno);
-			else {
-				mensagem("Aluno cadastrado com sucesso!");
+    private static void mensagem(String string) throws InterruptedException {
+        limparTela();
+        System.out.println(string);
+        espera(2);
+        limparTela();
+    }
 
-			}
-		} catch (Exception e) {
-			mensagem("Opção inválida, iniciando novo cadastro de notas");
-			capturaNotasAluno(aluno);
-		}
+    private static void espera(int secconds) throws InterruptedException {
+        Thread.sleep(secconds * 1000);
+    }
 
-	}
+    private static void mostrarAlunos() throws InterruptedException {
+        if (alunos.size() == 0) {
+            mensagem("Nenhum aluno cadastrado");
+            return;
+        }
 
-	private static void mensagem(String string) throws InterruptedException {
-		limparTela();
-		System.out.println(string);
-		espera(2);
-		limparTela();
-	}
+        System.out.println("======== [ Relatório de alunos ] ========");
+        for (Aluno aluno : alunos) {
+            System.out.println("Nome: " + aluno.getNome());
+            String notas = "";
+            for (float nota : aluno.getNotas()) {
+                notas += nota + ", ";
+            }
+            System.out.println("Notas: " + notas);
+            System.out.println("Média: " + aluno.media());
+            System.out.println("Situação: " + aluno.situacao());
+            System.out.println("-------------------------------");
+        }
 
-	private static void espera(int secconds) throws InterruptedException {
-		Thread.sleep(secconds * 1000);
+        espera(8);
+        limparTela();
+    }
 
-	}
+    private static void cadastroAluno() throws NumberFormatException, IOException, InterruptedException {
+        var aluno = new Aluno();
+        System.out.println("Digite o nome do aluno");
+        aluno.setNome(reader.readLine());
 
-	private static void mostrarAluno() throws InterruptedException {
-		if (alunos.size() == 0) {
-			mensagem("Nenhum Aluno cadastrado");
-			return;
-		}
-		System.out.println("=====[Relatório de alunos]=====");
-		for (Aluno aluno : alunos) {
-			System.out.println("Nome:" + aluno.getNome());
-			String notas = "";
-			for (float nota : aluno.getNotas()) {
-				notas += nota + ",";
-			}
-			System.out.println("Notas:" + notas);
-			System.out.println("Média:" + aluno.media());
-			System.out.println("Situação:" + aluno.situacao());
-			System.out.println("--------------------------------");
+        capturaNotasAluno(aluno);
 
-		}
+        alunos.add(aluno);
 
-		espera(2);
-		limparTela();
+        mensagem("Aluno cadastrado com sucesso!");
+    }
 
-	}
-
-	private static void cadastroAluno() throws NumberFormatException, IOException, InterruptedException {
-		var aluno = new Aluno();
-		System.out.println("Digite o nome do aluno");
-		aluno.setNome(reader.readLine());
-
-		capturaNotasAluno(aluno);
-
-	}
 }
